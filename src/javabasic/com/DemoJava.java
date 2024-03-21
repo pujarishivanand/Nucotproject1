ccdd74646f6fabd735ae432937d0d8841954dc6c
pujarishivanand <pujarishivanand1998@gmail.com>
2024-03-22 00:11:28 +0530

    first round of commit

---
+++ src/javabasic/com/DemoJava.java
@@ -0,0 +1,10 @@
+package javabasic.com;
+
+public class DemoJava {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello Automation");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Shivanand
+ *
+ */
+module Nucotproject {
+}
