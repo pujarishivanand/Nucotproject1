6c3344931250fba4ac0d41c207634e0025849503
pujarishivanand <pujarishivanand1998@gmail.com>
2024-03-24 17:04:03 +0530

    added SumOfNumber file and modified N_Edit_Comment file...

---
+++ src/javabasic/com/SumOfNumber.java
@@ -0,0 +1,16 @@
+package javabasic.com;
+
+public class SumOfNumber {
+
+	public static void main(String[] args) {
+
+		int a=10;
+		int b=20;
+		
+		int c=a+b;
+		
+		System.out.println(c);
+		
+	}
+
+}
